d37c743be346d44edb569c18a5ea22e1322e8f79
Aaron <aaronmckinstry706@gmail.com>
2017-08-14 21:55:57 -0400

    Script for structuring training data. Added dir definitions for train/valid/test. Part of training script.

--- src/definitions.py
+++ src/definitions.py
@@ -2,4 +2,10 @@ import os.path as path
 
 SRC_ROOT_DIR = path.dirname(path.abspath(__file__))
 PROJECT_ROOT_DIR = path.abspath(SRC_ROOT_DIR + '/..')
-UNIT_TEST_RESOURCE_ROOT_DIR = path.abspath(PROJECT_ROOT_DIR + '/unit_test_resources')
+UNIT_TEST_RESOURCE_ROOT_DIR = path.abspath(
+    PROJECT_ROOT_DIR + '/unit_test_resources')
+DATA_DIR = PROJECT_ROOT_DIR + '/data'
+VALIDATION_DATA_DIR = DATA_DIR + '/validation'
+TRAINING_DATA_DIR = DATA_DIR + '/training'
+TESTING_DATA_DIR = DATA_DIR + '/testing'
+PRETRAINING_DATA_DIR = DATA_DIR + '/pretraining'
--- src/run.py
+++ src/run.py
@@ -1,17 +1,102 @@
+import logging
+
 import lasagne
 import lasagne.layers as layers
+import lasagne.objectives as objectives
+import lasagne.regularization as regularization
+import lasagne.updates as updates
+import numpy
 import theano
 import theano.tensor as tensor
 
-# Create a dummy network.
-input = tensor.tensor4(name="image_batch")
-network = layers.InputLayer(
-    input_var=input, shape=(None, 3, 224, 224), name="input")
-network = layers.Conv2DLayer(
-    incoming=network, num_filters=2, filter_size=(3,3))
-network = layers.GlobalPoolLayer(incoming=network, pool_function=tensor.max)
-network = layers.NonlinearityLayer(incoming=network, nonlinearity=lasagne.nonlinearities.softmax)
-output = layers.get_output(layer_or_layers=network)
+import config.config as config
+import definitions as defs
+import utilities.utilities as utils
 
-network_function = theano.function([input], output)
-print(network_function([[[[0 for k in range(224)] for j in range(224)] for i in range(3)]]))
+logging.getLogger().setLevel(logging.INFO)
+
+metaparams = config.read_param_xml_file(defs.PROJECT_ROOT_DIR + "/config.xml")
+
+# Create a network, labels, outputs, and parameters with symbolic variables.
+
+input_batch = tensor.tensor4(name="image_batch")
+
+network = layers.InputLayer(
+    input_var=input_batch,
+    shape=(None, 3, metaparams['image_width'], metaparams['image_width']),
+    name="input")
+network = layers.Conv2DLayer(
+    incoming=network,
+    num_filters=2,
+    filter_size=(3,3))
+network = layers.GlobalPoolLayer(
+    incoming=network,
+    pool_function=tensor.max)
+network = layers.NonlinearityLayer(
+    incoming=network,
+    nonlinearity=lasagne.nonlinearities.softmax)
+
+training_output_batch = layers.get_output(network, deterministic=False)
+testing_output_batch = layers.get_output(network, deterministic=True)
+network_parameters = layers.get_all_params(network, trainable=True)
+label_batch = tensor.matrix(name="label_batch")
+
+# Create objective functions, gradient norm, learning rate, network updates. 
+
+training_loss_batch = tensor.sum(
+        objectives.categorical_crossentropy(training_output_batch,
+                                            label_batch)) / metaparams['batch_size'] \
+    + regularization.regularize_network_params(network, regularization.l2)
+
+validation_loss_batch = \
+    tensor.sum(
+        objectives.categorical_crossentropy(testing_output_batch, label_batch))
+
+gradients = tensor.concatenate(
+    [tensor.flatten(tensor.grad(training_loss_batch, params))
+     for params in layers.get_all_params(network, trainable=True)])
+gradient_norm = gradients.norm(2)
+
+learning_rate = theano.shared(numpy.float32(metaparams['learning_rate']))
+
+network_updates = updates.nesterov_momentum(
+    loss_or_grads=training_loss_batch,
+    params=network_parameters,
+    learning_rate=learning_rate,
+    momentum=metaparams['momentum_term'])
+
+# Compile training/validation/testing functions. 
+
+logging.info('Compiling training/validation functions.')
+train = theano.function(
+    inputs=[input_batch, label_batch],
+    outputs=[training_output_batch, training_loss_batch, gradient_norm])
+validate = theano.function(
+    inputs=[input_batch, label_batch],
+    outputs=[testing_output_batch, validation_loss_batch])
+
+# Reset leftover stuff from earlier runs. 
+
+logging.info('Cleaning up training/validation split from previous runs.')
+utils.recombine_validation_and_training(defs.VALIDATION_DATA_DIR,
+                                        defs.TRAINING_DATA_DIR)
+
+logging.info('Splitting training and validation images.')
+utils.separate_validation_set(defs.TRAINING_DATA_DIR,
+                              defs.VALIDATION_DATA_DIR,
+                              split=metaparams['validation_split'])
+
+# Keep record of various metrics as the network is training. 
+
+previous_validation_losses = []
+previous_training_losses = []
+gradient_norms = []
+
+# Use the Early Stopping technique for determining when to end training.
+
+best_validation_loss = float("inf")
+remaining_patience = metaparams['patience']
+
+# And now we train. 
+
+
